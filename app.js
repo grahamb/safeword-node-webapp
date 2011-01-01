25a0fd559d637a72a828cc7025f145c2ac3e7926
Graham Ballantyne <hello@grahamballantyne.com>
2011-01-01 12:24:11 -0800

    Re-tabbed, set up mongodb connection
    Signed-off-by: Graham Ballantyne <hello@grahamballantyne.com>

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
 var express = require('express')
-,	app = module.exports = express.createServer();
+,   app = module.exports = express.createServer()
+,	mongoose = require('mongoose').Mongoose
+,	db = mongoose.connect('mongodb://localhost/safeword')
+,	Word;
+
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyDecoder());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyDecoder());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.staticProvider(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.logger());
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+	app.use(express.logger());
+	app.use(express.errorHandler()); 
 });
+
+app.Word = Word = require('./models.js').Word(db);
 
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    locals: {
-      title: 'The Safeword'
-    }
-  });
+	res.render('index', {
+		locals: {
+			title: 'The Safeword'
+		}
+	});
 });
+
+
+
+
+
+
+
+
+
+
 
 // Only listen on $ node app.js
 
 if (!module.parent) {
-  app.listen(3000);
-  console.log("Express server listening on port %d", app.address().port);
+	app.listen(3000);
+	console.log("Express server listening on port %d", app.address().port);
 }
